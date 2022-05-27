f2851a5e55cb1c6b7147d78b385a48d48e21198f
Gabrielle <gaparecidapichau@gmail.com>
2022-05-27 10:31:46 -0300

    salvo

---
+++ src/components/BlocoPrincipaL.vue
@@ -0,0 +1,15 @@
+<script>
+export default {};
+</script>
+<template>
+  <main id="content">
+    <article class="block">ola mundo</article>
+    <article class="block">ola mundo</article>
+    <article class="block">ola mundo</article>
+    <article class="block">ola mundo</article>
+    <article class="block">ola mundo</article>
+    <article class="block">ola mundo</article>
+    <article class="blokead">ola mundo</article>
+  </main>
+</template>
+<style></style>
